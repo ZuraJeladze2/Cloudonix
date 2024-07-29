3c1e70f7ee606a1ff9e37088e2650fa3a5877180
ZuraJeladze2 <Zurajeladze2@gmail.com>
2024-07-30 01:07:44 +0400

    adding table view
    adding mat-table with fields: id, name, sku, cost.
    
    todo: add Iproduct interface so I don't use any type

--- src/app/pages/product-list/product-list.component.ts
+++ src/app/pages/product-list/product-list.component.ts
@@ -1,21 +1,48 @@
-import { AsyncPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild, AfterViewInit, OnInit } from '@angular/core';
 import { Observable, tap } from 'rxjs';
 import { ProductsService } from '../../core/services/products.service';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [MatTableModule, MatInputModule, MatPaginatorModule, MatFormFieldModule, MatSortModule],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.scss'
+  styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent {
-  productsService: ProductsService = inject(ProductsService)
+export class ProductListComponent implements AfterViewInit, OnInit {
+  productsService: ProductsService = inject(ProductsService);
   products$: Observable<any> = this.productsService.getProducts().pipe(
-    tap(x=> {
-      console.log(x);
-      
+    tap(products => {
+      console.table(products);
+      this.dataSource.data = products;
     })
-  )
+  );
+  displayedColumns: string[] = ['id', 'name', 'sku', 'cost'];
+  dataSource: MatTableDataSource<any> = new MatTableDataSource();
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngOnInit() {
+    this.products$.subscribe();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
