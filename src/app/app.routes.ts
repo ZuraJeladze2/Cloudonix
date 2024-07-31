c5e68be8dd5222acdc0839b2f29e98877900de46
ZuraJeladze2 <Zurajeladze2@gmail.com>
2024-07-31 22:19:19 +0400

    lazy loading components
    also adding conditional rendering of 'Custom properties' header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-import { ProductDetailsComponent } from './pages/product-details/product-details.component';
-import { ProductListComponent } from './pages/product-list/product-list.component';
 
+// loadComponent: () => import('./pages/user-create/user-create.component').then(x => x.UserCreateComponent)
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'products', component: ProductListComponent },
-  { path: 'products/:id', component: ProductDetailsComponent },
+  { path: 'login', loadComponent: () => import('./pages/login/login.component').then(x => x.LoginComponent) },
+  { path: 'products', loadComponent: () => import('./pages/product-list/product-list.component').then(x => x.ProductListComponent) },
+  { path: 'products/:id', loadComponent: () => import('./pages/product-details/product-details.component').then(x => x.ProductDetailsComponent) },
   { path: '**', redirectTo: '/login' }
 ];
